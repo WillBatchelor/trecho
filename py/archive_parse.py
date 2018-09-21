38f8c38557d1518f9e4817fb5298cc47b4f68a20
WillBatchelor <batchelorwd8@gmail.com>
2018-09-21 12:00:21 -0400

    Added json settings and fixed archive parser.

--- py/archive_parse.py
+++ py/archive_parse.py
@@ -4,8 +4,8 @@ from pprint import pprint
 with open("trello_dump.json") as json_str:
 	data = json.load(json_str)
 
+output_arch = [
+        x for x in data if "closed" in x["data"]["card"]
+]
 
-
-#for x in data:
-  #      if "closed" in x["data"]["card"]:
-              #  pprint(x["data"]["card"])               
+pprint(output_arch)
